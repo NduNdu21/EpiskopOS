eb3b5f7766eb2a7a34eae172021666479010acd7
Ndumiso Mbangeleli (Student) <220216407@aston.ac.uk>
2026-02-15 22:45:21 +0000

    Updating and pushing changes that didn't save yesterday

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,3 +16,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const pool = require("./config/db");
+
+pool.query("SELECT NOW()")
+  .then(res => console.log("DB connected:", res.rows[0]))
+  .catch(err => console.error("DB error:", err));
