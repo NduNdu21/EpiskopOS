a869bd379c86706cce9aa8d6559cf9272f822c69
Ndumiso Mbangeleli <220216407@aston.ac.uk>
2026-02-17 23:50:02 +0000

    Updating backend port

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,19 @@
 require("dotenv").config();
 const pool = require("./config/db");
+const express = require("express");
 
-pool.query("SELECT NOW()")
-  .then(res => console.log("DB connected:", res.rows[0]))
-  .catch(err => console.error("DB error:", err));
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, "0.0.0.0", async () => {
+  try {
+    const res = await pool.query("SELECT NOW()");
+    console.log("DB connected:", res.rows[0]);
+  } catch (err) {
+    console.error("DB error:", err);
+  }
+  
+  console.log(`Server running on http://0.0.0.0:${port}`);
+});
