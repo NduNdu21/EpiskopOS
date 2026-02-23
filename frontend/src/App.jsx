7c058b88298b96bcc9cab3a5a521e816ac07b231
Ndumiso Mbangeleli (Student) <220216407@aston.ac.uk>
2026-02-23 13:40:17 +0000

    Import router functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>EpiskopOS</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>EpiskopOS</h1>
+      </div>
+    </BrowserRouter>
   )
 }
 
