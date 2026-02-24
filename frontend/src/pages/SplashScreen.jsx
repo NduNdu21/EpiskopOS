be931734e41ae23eea4d46e4b002065393c435a1
Ndumiso Mbangeleli <220216407@aston.ac.uk>
2026-02-24 17:13:54 +0000

    Placeholder file for splashscreen ui

---
+++ frontend/src/pages/SplashScreen.jsx
@@ -0,0 +1,15 @@
+//import { useState } from 'react'
+
+function SplashScreen() {
+
+    //setting state of main page
+    //const [count, setCount] = useState(0)
+
+    return (
+        <div className='bg-gradient-to-b from-dark-teal to-ash-grey'>
+            
+        </div>
+    )
+}
+
+export default SplashScreen
