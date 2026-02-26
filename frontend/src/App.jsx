c4ca3beb8c9c752c9d810df84e1c1f2c1336e867
Ndumiso Mbangeleli (Student) <220216407@aston.ac.uk>
2026-02-26 22:37:32 +0000

    Router to register page on App file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './index.css'
 import './App.css'
+import Register from "./pages/Register";
 
 function App() {
 
@@ -9,6 +10,9 @@ function App() {
       <div className="flex bg-gradient-to-b from-dark-teal to-ash-grey">
         <h1 className="text-beige">EpiskopOS</h1>
       </div>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </BrowserRouter>
   )
 }
