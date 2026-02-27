076ce82b9ea2386ec782c516a508a29ba0690e3e
Ndumiso Mbangeleli <220216407@aston.ac.uk>
2026-02-27 00:01:52 +0000

    Remove url verification console statement

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,5 @@
 const { Pool } = require("pg");
 
-//verify database url
-console.log("Effective DATABASE_URL =", process.env.DATABASE_URL);
-
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is missing in .env");
 }
