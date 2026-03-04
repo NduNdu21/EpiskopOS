03d7b1da40a2d66d379386854d1164ead964087a
Ndumiso Mbangeleli <220216407@aston.ac.uk>
2026-03-04 01:31:03 +0000

    Server updated with cors for errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const pool = require("./config/db");
 const express = require("express");
+const cors = require("cors");
+
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(cors()); 
 app.use(express.json());
 
 // Register your routes
