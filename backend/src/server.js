a73416e86e8072c3266d8eba8504765dff47f48c
Ndumiso Mbangeleli <220216407@aston.ac.uk>
2026-03-06 00:06:57 +0000

    Register event route to server file

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,9 +11,12 @@ const port = process.env.PORT || 5000;
 app.use(cors({ origin: "http://localhost:5173", credentials: true})); 
 app.use(express.json());
 
-// Register your routes
+// Register routes
 const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
+
+const eventRoutes = require("./routes/eventRoutes");
+app.use("/api/events", eventRoutes);
 
 // Start server + test DB
 app.listen(port, "0.0.0.0", async () => {
