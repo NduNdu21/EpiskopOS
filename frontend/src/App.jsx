4d84f7af2955e0a3bd42fedf553c84d1a26ddf15
Ndumiso Mbangeleli <220216407@aston.ac.uk>
2026-03-08 22:28:34 +0000

    Removing unused elements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
 import './App.css'
 import Register from "./pages/Register";
@@ -8,9 +8,6 @@ function App() {
 
   return (
     <BrowserRouter>
-      <div className="flex bg-gradient-to-b from-dark-teal to-ash-grey">
-        <h1 className="text-beige">EpiskopOS</h1>
-      </div>
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />}/>
