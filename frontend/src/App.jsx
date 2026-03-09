37587c1af3e235a9b73de5306ca8a9dc213dc22d
Ndumiso Mbangeleli <220216407@aston.ac.uk>
2026-03-09 21:40:58 +0000

    Protecting home page so that blocked by login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,16 @@ import './index.css'
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import HomePage from './pages/HomePage';
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={
+          <ProtectedRoute><HomePage /></ProtectedRoute>
+        } />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />}/>
       </Routes>
