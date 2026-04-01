eb44016fe9f2b205f334bbf59f673179049c60f1
Ndumiso Mbangeleli <220216407@aston.ac.uk>
2026-04-01 20:46:58 +0000

    Resolving errors

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 const { getMe, getUsers, updateUserRole, deleteUser } = require("../controllers/userController");
 
+const userCtrl = require('../controllers/userController');
+console.log('userCtrl exports:', userCtrl);
+
 router.get("/me", authMiddleware, getMe);
 router.get("/", authMiddleware, getUsers);
 router.patch("/:id/role", authMiddleware, updateUserRole);
