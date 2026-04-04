29d896d2a511f9e4361c2357b648ba331a47675d
Ndumiso Mbangeleli <220216407@aston.ac.uk>
2026-04-04 17:18:36 +0000

    Resolved error accessing live routes

--- backend/src/routes/eventRoutes.js
+++ backend/src/routes/eventRoutes.js
@@ -24,15 +24,16 @@ const {
 // All routes require login
 router.use(authMiddleware);
 
+//Live service routes must be before /:id routes
+router.get("/live", getLiveEvent);
+router.get("/current", getCurrentAndNext);
+
 //Main event routes
 router.get("/", getEvents);                           // all users
+router.get("/", getMyEvents);                         // volunteers
 router.post("/", createEvent);                        // admin
 router.put("/:id", updateEvent);                      // admin
 router.delete("/:id", deleteEvent);                   // admin
-router.get("/", getMyEvents);                         // volunteers
-
-//Home page display route
-router.get("/current", getCurrentAndNext);            // all users
 
 //Event segment routes
 router.get("/:id/segments", getSegments);             // all users
@@ -45,7 +46,6 @@ router.post("/:id/segments/:segmentId/teams", addSegmentTeam);    //admin
 router.delete("/:id/segments/:segmentId/teams/:team", removeSegmentTeam);    //admin
 
 // Live service routes
-router.get("/live", getLiveEvent);
 router.post("/:id/golive", goLive);
 router.post("/:id/next", nextSegment);
 router.post("/:id/prev", prevSegment);
