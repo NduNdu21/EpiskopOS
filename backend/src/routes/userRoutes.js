b5fda4b581884a323e876ee6a3f424aded4f744b
Ndumiso Mbangeleli <ndumisobm@icloud.com>
2026-08-11 00:55:52 +0100

    Routing new api endpoints

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
-const { getMe, getUsers, updateUserRole, approveUser, deleteUser } = require("../controllers/userController");
+const { getMe, getUsers, updateUserRole, approveUser, deleteUser, updatePassword, updateProfile } = require("../controllers/userController");
 
 
 router.get("/me", authMiddleware, getMe);
+router.patch("/me", authMiddleware, updateProfile);
 router.get("/", authMiddleware, getUsers);
+router.patch("/me/password", authMiddleware, updatePassword);
 router.patch("/:id/role", authMiddleware, updateUserRole);
 router.patch("/:id/approve", authMiddleware, approveUser);
 router.delete("/:id", authMiddleware, deleteUser);
