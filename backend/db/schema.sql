8bb5ed2fd1b00f0d8924c5314eb9841f0c715be6
Ndumiso Mbangeleli <ndumisobm@icloud.com>
2026-08-18 17:38:48 +0100

    Fix remaining schema.sql issues for CI compatibility

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -20,7 +20,7 @@ SET row_security = off;
 -- Name: public; Type: SCHEMA; Schema: -; Owner: -
 --
 
-CREATE SCHEMA public;
+CREATE SCHEMA if not exists public;
 
 
 --
@@ -59,7 +59,7 @@ CREATE TYPE public.user_role AS ENUM (
     'admin',
     'lighting',
     'sound',
-    'media',
+    'media'
 );
 
 
@@ -191,11 +191,11 @@ CREATE TABLE public.segment_teams (
 --
 
 CREATE TABLE public.users (
-    id uuid DEFAULT extensions.uuid_generate_v4() NOT NULL,
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
     name character varying(100) NOT NULL,
     email character varying(150) NOT NULL,
     password_hash text NOT NULL,
-    role public.user_role DEFAULT 'volunteer'::public.user_role NOT NULL,
+    role public.user_role NOT NULL,
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     status text DEFAULT 'approved'::text NOT NULL,
     organization_id uuid NOT NULL
